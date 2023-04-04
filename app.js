8218ab067d60cda8c674d8edc1314e6262b726ff
stephen1521 <stephen.warnock@codeimmersives.com>
2023-04-04 11:10:42 -0400

    added connection through mongoose and auth for user

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+//loads the contents of config.env
+require("dotenv").config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// const { mongoConnect } = require('./mongo.js');
+// mongoConnect();
+const { mongooseConnect } = require('./mongoose.js')
+mongooseConnect();
+
+const app = express();
+
+//add CORS middleware 
+app.use(cors());
+app.options("*", cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
